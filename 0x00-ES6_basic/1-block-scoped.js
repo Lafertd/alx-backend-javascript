b011882f88586fb34013d9b7240ed42caa03a59f
lafertd <achrafprofessionalinfo@gmail.com>
2024-10-14 20:54:55 +0100

    up

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
+  return [task, task2];
 }
 
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
-
-  return [task, task2];
