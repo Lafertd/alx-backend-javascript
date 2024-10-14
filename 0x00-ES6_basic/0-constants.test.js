e251077e3b045fca31178c24293e126dce0fc99a
lafertd <achrafprofessionalinfo@gmail.com>
2024-10-14 16:59:41 +0100

    success

---
+++ 0x00-ES6_basic/0-constants.test.js
@@ -0,0 +1,6 @@
+// 0-constants.test.js
+import { taskFirst, taskNext } from './0-constants.js';
+
+test('tasks are correctly defined', () => {
+  expect(`${taskFirst()} ${taskNext()}`).toEqual("I prefer const when I can. But sometimes let is okay");
+});
